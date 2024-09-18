4bc44dbcdfa3c9e3d3c5189d25ada6bcbc1cde1d
Muhammadqodir <muhammadqodirshuhratov04@gmail.com>
2024-09-18 15:55:05 +0500

    show applications for manager

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,35 +12,44 @@
                     @if (auth()->user()->role->name == 'manager')
                         <span class='text-blue-500 font-bold text-xl'>Received Applications</span>
                         <div class='mt-5'>
-                            <div class="rounded-xl border p-5 mt-5 shadow-md w-9/12 bg-white">
-                                <div class="flex w-full items-center justify-between border-b pb-3">
-                                    <div class="flex items-center space-x-3">
-                                        <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]">
+                            @foreach ($applications as $application)
+                                <div class="rounded-xl border p-5 mt-5 shadow-md w-9/12 bg-white">
+                                    <div class="flex w-full items-center justify-between border-b pb-3">
+                                        <div class="flex items-center space-x-3">
+                                            <div class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]">
+                                            </div>
+                                            <div class="text-lg font-bold text-slate-700"> {{ $application->user->name }} </div>
                                         </div>
-                                        <div class="text-lg font-bold text-slate-700">Joe Smith</div>
+                                        <div class="flex items-center space-x-8">
+                                            <button class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">
+                                                # {{ $application->id }}
+                                            </button>
+                                            <div class="text-xs text-neutral-500"> {{ $application->created_at }} </div>
+                                        </div>
                                     </div>
-                                    <div class="flex items-center space-x-8">
-                                        <button class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">id:</button>
-                                        <div class="text-xs text-neutral-500">2 hours ago</div>
+                                    <div class="mt-4 mb-3">
+                                        <div class="mb-3 text-xl font-bold"> {{ $application->subject }} </div>
+                                        <div class="text-sm text-neutral-600"> {{ $application->message }} </div>
+                                    </div>
+                                    <div class="flex justify-between">
+                                        <div class="flex items-center justify-between text-slate-500">
+                                            {{ $application->user->email }}
+                                        </div>
+                                        <div class="border p-2 hover:bg-gray-200 transition cursor-pointer">
+                                            @if ($application->file_url !== null)
+                                                <a href="{{ asset('storage/' . $application->file_url) }}" target="_blank">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                                    </svg>
+                                                </a>
+                                            @else
+                                                No File
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="mt-4 mb-3">
-                                    <div class="mb-3 text-xl font-bold">
-                                        Nulla sed leo tempus, feugiat velit vel, rhoncus neque?
-                                    </div>
-                                    <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna.
-                                        Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed
-                                        gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum
-                                        consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec.
-                                        Curabitur eget feugiat massa
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="flex items-center justify-between text-slate-500">
-                                        email
-                                    </div>
-                                </div>
-                            </div>
+                            @endforeach
+                            {{ $applications->links() }}
                         </div>
                     @elseif (auth()->user()->role->name == 'client')
                         <div class='flex items-center'>
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,20 @@
 <?php
 
 use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect('dashboard');
-})->middleware('auth');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/', [MainController::class, 'main'])->name('main');
+    Route::get('/dashboard', [MainController::class, 'dashboard'])->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    // Route::resource('profiles', ProfileController::class);
 
-Route::resource('applications', ApplicationController::class);
+    Route::resource('applications', ApplicationController::class);
+});
 
 require __DIR__.'/auth.php';
